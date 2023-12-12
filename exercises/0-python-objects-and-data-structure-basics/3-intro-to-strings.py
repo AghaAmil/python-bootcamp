67dc8560e9519ddbcded2ea09de3ee71d60a069f
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-12-12 20:53:21 +0330

    python strings basics

---
+++ exercises/0-python-objects-and-data-structure-basics/3-intro-to-strings.py
@@ -0,0 +1,18 @@
+# creating a new string
+
+print('Hello World!')
+print("creating a string using double quote")
+print("I'm Amirhossein Moravveji")
+
+# blank line
+print("")
+
+# break line
+print('This is the first line\nThis is the second line')
+print('This is the third line')
+print('Check the code to see the differences between them\n')
+
+# print length of the string
+print(len('Hello World!'))
+
+
