69cc3ea5199feb778c299908cead3f7836944040
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-12-14 22:27:05 +0330

    string indexing and slicing

--- exercises/0-python-objects-and-data-structure-basics/3-intro-to-strings.py
+++ exercises/0-python-objects-and-data-structure-basics/3-intro-to-strings.py
@@ -12,7 +12,50 @@ print('This is the first line\nThis is the second line')
 print('This is the third line')
 print('Check the code to see the differences between them\n')
 
+# using tab between two words
+print('Amir \t Moravveji \t')
+
 # print length of the string
 print(len('Hello World!'))
 
+# blank line
+print("")
 
+# string indexing
+string_1 = 'Amirhossein Moravveji'
+print(string_1)
+print(len(string_1))
+
+# first letter
+print(string_1[0])
+# last letter
+print(string_1[20])     # last index/letter = len(variable) - 1  --> 21 - 1 = 20
+print(string_1[-1])     # another way to get last index/letter
+# something in between
+print(string_1[6])
+
+# string slicing
+
+# starting from an index all the way to the end
+print(string_1[3:])
+# staring from the beginning all the way to a specific index
+print(string_1[:14])    # note that the last index is not included
+# selecting a range of indexes
+print(string_1[4:11])
+
+# include steps from beginning to the end
+print(string_1[::2])
+print(string_1[::3])
+
+print(string_1[2:15:2])
+
+# using a python trick the reverse the string
+print(string_1[::-1])
+
+# blank line
+print("")
+
+# another example
+string_2 = 'abcdefghijklmnopqrst'
+print(string_2)
+print(string_2[::-1])
