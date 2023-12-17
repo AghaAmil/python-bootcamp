8783808cf48e84d21fcdb5c4dacce0e10632193d
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-12-17 22:21:06 +0330

    string formatting - part 2

--- exercises/0-python-objects-and-data-structure-basics/4-string-formatting.py
+++ exercises/0-python-objects-and-data-structure-basics/4-string-formatting.py
@@ -30,3 +30,49 @@ print('')
 print('The number PI in mathematics is equal to %s' % 3.14)
 print('The number PI in mathematics is equal to %d' % 3.14)
 
+# blank line
+print('')
+
+# Padding and Precision of Floating Point Numbers
+print('This is a float number: %4.2f' % 34.1555)
+print('This is a float number: %5.2f' % 34.1555)
+print('This is a float number: %6.2f' % 34.1555)
+print('This is a float number: %10.2f' % 34.1555)
+print('This is a float number: %5.4f' % 34.1555)
+print('This is a float number: %.4f' % 34.1555)
+print('This is a float number: %.0f' % 34.1555)
+print('This is a float number: %1.0f' % 34.1555)
+
+# blank line
+print('')
+
+# multiple formatting
+print('First: %s, Second: %5.2f, Third: %r,' % ('number', 77.123, 'COOL'))
+
+# blank line
+print('')
+
+# Formatting with the .format() method
+print('Hello, My name is {}'.format('amirhossein'))
+print('Hello, My complete name is {} {}'.format('Amirhossein', 'Moravveji'))
+
+user_name = 'kecy1amr'
+print('My user name is {}'.format(user_name))
+
+print('The {} {} {}'.format('fox', 'brown', 'quick'))
+print('The {2} {1} {0}'.format('fox', 'brown', 'quick'))
+print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+print('The {q} {b} {f}. The {q} red {f}'.format(f='fox', b='brown', q='quick'))
+
+# blank line
+print('')
+
+print('{0:8} | {1:9}'.format('Fruits', 'Quantity'))
+print('{0:8} | {1:9}'.format('Apple', 3.))
+print('{0:8} | {1:9}'.format('Orange', 10))
+
+# blank line
+print('')
+
+print('{0:<10} | {1:^10} | {2:>10}'.format('Left', 'Center', 'Right'))
+print('{0:<10} | {1:^10} | {2:>10}'.format(11, 22, 33))
