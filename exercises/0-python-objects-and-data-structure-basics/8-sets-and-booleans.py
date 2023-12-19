03f27cf7123881aa5654c152cbb307a71a7de4de
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-12-19 20:26:55 +0330

    python sets

---
+++ exercises/0-python-objects-and-data-structure-basics/8-sets-and-booleans.py
@@ -0,0 +1,24 @@
+# let's create a set
+my_set = set()
+
+print(my_set)
+
+my_set.add(1)
+my_set.add(2)
+my_set.add(4)
+
+print(my_set)
+
+# add another repetitive number
+my_set.add(4)
+
+print(my_set)
+
+# add another unique object to our set
+my_set.add(3)
+
+print(my_set)
+
+my_list = [1, 1, 1, 2, 3, 4, 5, 4, 4, 4, 5, 6, 4, 3, 3, 2, 4, 3, 4]
+print(my_list)
+print(set(my_list))
