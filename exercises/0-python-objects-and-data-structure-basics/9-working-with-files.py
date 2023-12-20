f3dfde92a0d62fccf9ef6b199b54349afa6bb69a
AghaAmil <moravveji.amir.92@gmail.com>
2023-12-20 22:14:10 +0330

    working with files - part 1

---
+++ exercises/0-python-objects-and-data-structure-basics/9-working-with-files.py
@@ -0,0 +1,28 @@
+# opening a python file
+my_file = open('sample-file.txt')
+
+print('The content of the file is displayed below: \n')
+print(my_file.read())
+
+# blank line
+print('')
+
+print('Calling the .read() method again will result an empty string: \n')
+print(my_file.read())
+
+# reset cursor
+my_file.seek(0)
+
+print('We have reset the position of the contents of the file, we read the file again: \n')
+print(my_file.read())
+
+# blank line
+print('')
+
+# reset cursor
+my_file.seek(0)
+
+print('Store the content of file line-by-line in a variable and then print it: \n')
+file_content = my_file.readline()
+
+print(file_content)
