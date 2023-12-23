ee2ffc99d3206d4171d3747a5eb456b0ddbcd058
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2023-12-23 19:57:37 +0330

    remove extra spaces in result

--- exercises/0-python-objects-and-data-structure-basics/9-working-with-files.py
+++ exercises/0-python-objects-and-data-structure-basics/9-working-with-files.py
@@ -1,19 +1,19 @@
 # opening a python file
-my_file = open('sample-file.txt')
+my_file = open('../sample-python-files/sample-file.txt')
 
-print('The content of the file is displayed below: \n')
+print('The content of the file is displayed below:')
 print(my_file.read())
 
 # blank line
 print('')
 
-print('Calling the .read() method again will result an empty string: \n')
+print('Calling the .read() method again will result an empty string:')
 print(my_file.read())
 
 # reset cursor
 my_file.seek(0)
 
-print('We have reset the position of the contents of the file, we read the file again: \n')
+print('We have reset the position of the contents of the file, we read the file again:')
 print(my_file.read())
 
 # blank line
@@ -22,7 +22,7 @@ print('')
 # reset cursor
 my_file.seek(0)
 
-print('Store the content of file line-by-line in a variable and then print it: \n')
+print('Store the content of file line-by-line in a variable and then print it:')
 file_content = my_file.readline()
 
 print(file_content)
