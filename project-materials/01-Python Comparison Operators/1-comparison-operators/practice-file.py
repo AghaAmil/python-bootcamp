d5d49c38e24c5b1ad4217601a0fbc7dc6151be1f
AghaAmil <moravveji.amir.92@gmail.com>
2024-01-11 22:15:59 +0330

    python comparison operators

---
+++ project-materials/01-Python Comparison Operators/1-comparison-operators/practice-file.py
@@ -0,0 +1,10 @@
+# represent some comparison operators example
+
+# type matters
+print('2' == 2)
+
+# capitalization matters
+print('Hello' == 'hello')
+
+# different type, but same amount in terms of mathematics
+print(2.0 == 2)
