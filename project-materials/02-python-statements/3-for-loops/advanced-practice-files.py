e19e14ae5dc54bc557c3b7bee33ae2d8f2959d24
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-01-17 18:59:12 +0330

    python for loops advanced - final

--- project-materials/02-python-statements/3-for-loops/advanced-practice-files.py
+++ project-materials/02-python-statements/3-for-loops/advanced-practice-files.py
@@ -16,3 +16,74 @@ for letter in zen:
         # ends the print output with no space
         # letter will be printed side by side
         print(letter, end='')
+
+# print blank lines
+print('\n')
+
+# Using "for" with a Tuple
+print('Print the sum of the values in the tuple below using for loop:')
+
+numbers = (34, 54, 67, 21, 78, 97, 45, 44, 80, 19)
+print(numbers)
+
+sum = 0
+
+for nums in numbers:
+    sum += nums
+
+print(f'The sum of the numbers in the tuples is: {sum}')
+
+# Using "for" with a Range Object
+sample_num = range(5)
+print(list(sample_num))
+
+sample_num2 = range(10, 20)
+print(list(sample_num2))
+
+sample_num3 = range(1, 10, 2)
+print(list(sample_num3))
+
+# Using 'for' loop
+for num in range(5):
+    print(num, end=' ')
+print('')
+
+for num in range(10, 20):
+    print(num, end=' ')
+print('')
+
+for num in range(1, 10, 2):
+    print(num, end=' ')
+
+# blank lines
+print('\n')
+print('We use the range() function to get the sequence of numbers from 1 to n-1 and perform cumulative '
+      'multiplication to get the factorial value.')
+
+fact = 1
+n = 5
+
+for num in range(1, n + 1):
+    fact *= num
+
+print(f'The factorial of 5! = {fact}')
+
+# blank lines
+print('\n')
+
+# Using "for" Loop with Sequence Index
+sample_num4 = [34, 54, 67, 21, 78, 45, 12]
+range_value = range(len(sample_num4))
+
+for items in range_value:
+    print(f'The Index: {items}, Number: {sample_num4[items]}')
+
+# blank lines
+print('\n')
+
+# Using "for" with Dictionaries
+numbers = {10: "Ten", 20: "Twenty", 30: "Thirty", 40: "Forty"}
+print(numbers)
+
+for x in numbers:
+    print(f'{x} : {numbers[x]}')
