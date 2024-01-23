33fc0b3b1dc345134735805ce88a6888bd09d0c6
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-01-23 19:39:05 +0330

    adding a while code challenge

--- project-materials/02-python-statements/4-while-loops/while-loop-external-material.py
+++ project-materials/02-python-statements/4-while-loops/while-loop-external-material.py
@@ -32,3 +32,21 @@ while counter1 < 5:
 else:
     print('While loop over. Now in else block')
 print('End of while loop')
+
+# bank lines
+print()
+print()
+
+# A code challenge
+i = 2
+while i < 100:
+    j = 2
+    while j <= (i / j):
+        if not (i % j):
+            break
+        j += 1
+    if j > i / j:
+        print(i, " is prime")
+    i += 1
+
+print("Good bye!")
