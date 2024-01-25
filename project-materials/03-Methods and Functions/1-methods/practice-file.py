f6bfe3cd009e5bb3099976961da8380bfa65d14b
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-01-25 21:53:06 +0330

    intro to python methods

---
+++ project-materials/03-Methods and Functions/1-methods/practice-file.py
@@ -0,0 +1,12 @@
+# Let's create a list
+sample_list = [1, 2, 3, 4, 4, 5, 5, 5, 5, 6, 7, 8, 9]
+print(sample_list)
+
+# append number 10
+sample_list.append(10)
+print(sample_list)
+
+# break line
+print()
+
+help(sample_list.append)
