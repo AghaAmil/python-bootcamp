ec6a0fc8c6dbf993c38b47c9659cce84e0b5afc4
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-02-02 18:35:56 +0330

    improvement on interaction between python functions

--- project-materials/03-Methods and Functions/2-functions/practice-file-3.py
+++ project-materials/03-Methods and Functions/2-functions/practice-file-3.py
@@ -2,9 +2,10 @@ from random import shuffle
 
 # check and test the shuffle function
 sample_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-shuffle(sample_list)
+print(f'Original list: {sample_list}')
 
-print(sample_list)
+shuffle(sample_list)
+print(f'Shuffled list: {sample_list}')
 
 
 # create my own shuffle function
@@ -14,13 +15,20 @@ def shuffle_list(l):
 
 
 # test the function
-print(shuffle_list(sample_list))
+# in this situation we can assign the result of the shuffle function to a variable
+mixed_list = shuffle_list(sample_list)
+
+print(f'Creating my own shuffle function: {mixed_list}')
+
+# blank line
+print()
 
 
 def get_user_input():
     user_input = ''
 
     while user_input not in ['0', '1', '2']:
+        print('Please enter a valid number below:')
         user_input = input("Choose a number from 0, 1, or 2: ")
 
     return int(user_input)
