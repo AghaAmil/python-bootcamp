020b23f76d7421dd428eafad817c76e6a1f88fad
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-03-20 21:54:18 +0330

    python variable assignments + file changes

---
+++ practice-files-and-projects/00-python-objects-and-data-structure-basics/2-variable-assignments/practice-variable-assignments.py
@@ -0,0 +1,102 @@
+"""
+Variable assignments
+
+"""
+
+# numbers
+my_number = 10
+print(my_number)
+print(type(my_number))
+
+# insert blank line
+print()
+
+# float
+my_float = 3.45
+print(my_float)
+print(type(my_float))
+
+# insert blank line
+print()
+
+# string
+my_string = "Amirhossein Moravveji"
+print(my_string)
+print(type(my_string))
+
+# insert blank line
+print()
+
+# list
+my_list = ['Amir', 39, 53.34, 'Moravveji']
+print(my_list)
+print(type(my_list))
+
+# insert blank line
+print()
+
+my_dictionary = {
+    'first_name': 'Amir',
+    'last_name': 'Moravveji',
+    'age': 100
+}
+print(my_dictionary)
+print(type(my_dictionary))
+
+# insert blank line
+print()
+
+my_tuple = (19, 'Moravveji', 745, {'first_name': 'Amir', 'last_name': 'moravveji', 'age': 100})
+print(my_tuple)
+print(type(my_tuple))
+
+# insert blank line
+print()
+
+my_set = {10, 20, 'Amir'}
+print(my_set)
+print(type(my_set))
+
+# insert blank line
+print()
+
+is_true = True
+is_false = False
+print(is_true)
+print(is_false)
+print(type(is_true))
+print(type(is_false))
+
+# insert blank line
+print()
+
+# increment & decrement
+num = 1
+print(num)
+
+num += 7
+print(num)
+
+num -= 3
+print(num)
+
+num *= 5
+print(num)
+
+num /= 3
+print(num)
+
+# insert blank line
+print()
+
+# Example of tax calculation
+income = 4500
+tax_percentage = 20
+
+income_after_tax = income - (income * tax_percentage) / 100
+
+print(income)
+print(tax_percentage)
+print(income_after_tax)
+
+
