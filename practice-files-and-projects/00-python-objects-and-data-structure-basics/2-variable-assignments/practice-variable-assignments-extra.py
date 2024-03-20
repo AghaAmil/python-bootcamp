1476edf4747a3392d4f52d4ca38958a239626106
Amirhossein Moravveji <54990581+AghaAmil@users.noreply.github.com>
2024-03-20 21:54:42 +0330

    python variable assignments extra topics

---
+++ practice-files-and-projects/00-python-objects-and-data-structure-basics/2-variable-assignments/practice-variable-assignments-extra.py
@@ -0,0 +1,27 @@
+"""
+Extra practice code on variable-assignments
+
+from https://www.tutorialspoint.com/python/python_variables.htm
+
+"""
+
+# casting python variables
+x = int(10)
+y = str(10)
+z = float(10)
+
+print('The x = ', x)
+print('The y = ', y)
+print('The z = ', z)
+
+# blank line
+print()
+
+# python multiple assignment
+a = b = c = 20
+print(a, b, c)
+
+# another method
+a, b, c, = 10, 11, 'Amir'
+print(a, b, c)
+
