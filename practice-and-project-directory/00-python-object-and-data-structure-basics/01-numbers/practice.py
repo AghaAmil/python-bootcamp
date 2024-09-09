2b069d3e916bda23f8331488b54514a1c6c25146
AghaAmil <moravveji.amir.92@gmail.com>
2024-09-09 14:08:20 +0330

    practice python numbers

---
+++ practice-and-project-directory/00-python-object-and-data-structure-basics/01-numbers/practice.py
@@ -0,0 +1,11 @@
+# The print() function prints the specified message to the screen, or other standard output device.
+print(1 + 2)
+print(3 - 1)
+print(8 * 4)
+print(7 / 3)
+
+print(2 ** 3)
+print(10 ** 0.5)
+
+print((3 + 4) * (11 - 2))
+print(543 % 81)
