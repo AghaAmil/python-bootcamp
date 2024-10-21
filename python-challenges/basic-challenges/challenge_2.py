c84f9ddfd8886667a75abfe22ca443d9766270f2
AghaAmil <moravveji.amir.92@gmail.com>
2024-10-21 20:15:22 +0330

    challenge - prime number generator

--- python-challenges/basic-challenges/challenge_2.py
+++ python-challenges/basic-challenges/challenge_2.py
@@ -9,3 +9,25 @@ Write a program that generates prime numbers up to a given limit using a for loo
 4. Print out all prime numbers found
 """
 
+
+def prime_checker(num):
+    if num < 2:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, num + 1):
+            if num % i == 0:
+                return False
+            else:
+                return True
+
+
+number_limit = int(input('Enter the upper limit: '))
+primes_container = []
+
+for num in range(2, number_limit + 1):
+    if prime_checker(num):
+        primes_container.append(num)
+
+print(f'List of the Prime Numbers up to {number_limit}: {primes_container}')
